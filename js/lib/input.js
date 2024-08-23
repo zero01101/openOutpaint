e3e24e76d48d0a11f5bea614b881da6f077eed10
Victor Seiji Hariki <victorseijih@gmail.com>
2024-08-23 00:43:17 -0300

    [Bug]: Repeated keyboard shortcuts ignored Fixes #292

--- js/lib/input.js
+++ js/lib/input.js
@@ -660,6 +660,9 @@ window.onkeyup = (evn) => {
 		});
 	}
 
+	if (keyboard.keys[evn.code]._hold_to)
+		clearTimeout(keyboard.keys[evn.code]._hold_to);
+
 	keyboard.keys[evn.code] = {
 		pressed: false,
 		held: false,
